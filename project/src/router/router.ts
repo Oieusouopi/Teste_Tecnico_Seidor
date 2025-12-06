5488844c20e8d8f160b9d99e8c43da032166b3fa
Oieusouopi <rafasc866@gmail.com>
2025-12-06 19:45:55 -0300

    rota principal

--- project/src/router/router.ts
+++ project/src/router/router.ts
@@ -1,3 +1,8 @@
 import { Router } from 'express';
+import automovelRouter from './automovelRouter';
 
 const router = Router();
+
+router.use("/automovel", automovelRouter);
+
+export default router;
