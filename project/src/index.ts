58f2013c3d7a9556f917d3400df8167f754bccea
Oieusouopi <rafasc866@gmail.com>
2025-12-06 20:15:47 -0300

    colocando router e json se precisar da parser

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from 'express';
+import router from './router/router';
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(express.json())
+
 app.get("/", (request: Request, response: Response) => {
     response.status(200).send("Hello World");
 });
+
+app.use(router);
 
 app.listen(PORT, () => {
     console.log("Server ruuning at PORT: ", PORT);
