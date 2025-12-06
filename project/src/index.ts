ba68480fcd181307fb9aa3e8042a2da1c785ce8e
Oieusouopi <rafasc866@gmail.com>
2025-12-06 18:26:38 -0300

    ajuste no dockerfile para debugger e tsconfig

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import express, { Request, Response } from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", (request: Request, response: Response) => {
+    response.status(200).send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log("Server ruuning at PORT: ", PORT);
+}).on("error", (error) => {
+    throw new Error(error.message);
+})
